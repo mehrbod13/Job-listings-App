f630c63d0412bce98a194c435f798ff785956eb0
mehrbod13 <mehrbod13@users.noreply.github.com>
2026-09-05 16:33:54 +0000

    deploy: bdad6aa33f205e776a4b7266bc78d398b185ab23

---
+++ _next/static/oleUkEzx3Nqt-uBRwOlec/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-84c20acbc384dd16.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/layout":["static/chunks/pages/layout-f72e85167f22edad.js"],sortedPages:["/","/_app","/_error","/layout"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
