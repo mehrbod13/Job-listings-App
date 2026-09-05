707f6e21af2e87a612982ab9015296a690dfff3c
mehrbod <100156835+mehrbod13@users.noreply.github.com>
2026-09-05 20:02:54 +0330

    Refactor next.config.js
    Removed unnecessary line breaks and simplified config structure.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  output: 'export',
-
   basePath: '/Job-listings-App',
   assetPrefix: '/Job-listings-App/',
-
-  images: {
-    unoptimized: true,
-  },
-
+  images: { unoptimized: true },
   trailingSlash: true,
 }
-
 module.exports = nextConfig
