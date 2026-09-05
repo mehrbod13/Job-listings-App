e2c47bc4dc923045974e1bd1a663da97088ccd89
mehrbod <100156835+mehrbod13@users.noreply.github.com>
2026-09-05 19:52:24 +0330

    deploy on github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: 'export',
+
+  basePath: '/Job-listings-App',
+  assetPrefix: '/Job-listings-App/',
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
